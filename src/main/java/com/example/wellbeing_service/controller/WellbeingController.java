b093d914bcbfe64d3f121198ff8d668164cfb94e
silvercen <abelkm123@gmail.com>
2024-11-08 17:40:32 +0530

    Added WellbeingController

--- src/main/java/com/example/wellbeing_service/controller/WellbeingController.java
+++ src/main/java/com/example/wellbeing_service/controller/WellbeingController.java
@@ -1,9 +1,34 @@
 package com.example.wellbeing_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.wellbeing_service.model.WellbeingModel;
+import com.example.wellbeing_service.service.WellbeingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wellbeing")
 public class WellbeingController {
+
+    @Autowired
+    private WellbeingService wellbeingService;
+
+    @PostMapping
+    public String saveWellbeing(@RequestBody WellbeingModel wellbeingModel) {
+        return wellbeingService.saveWellbeing(wellbeingModel);
+    }
+
+    @GetMapping("/{id}")
+    public WellbeingModel getWellbeing(@PathVariable String wellbeingId) {
+        return wellbeingService.getWellbeing(wellbeingId);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteWellbeing(@PathVariable String wellbeingId) {
+        return wellbeingService.deleteWellbeing(wellbeingId);
+    }
+
+    @GetMapping("/date")
+    public Iterable<WellbeingModel> getWellbeingByDate(@RequestParam String startDate, @RequestParam String endDate) {
+        return wellbeingService.getWellbeingByDate(startDate, endDate);
+    }
 }
